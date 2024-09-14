224a3de6fe5daaa8b0d17985ffa988ee1fc4525f
Thiago Lopes <thiagoslopes8@outlook.com>
2024-09-14 00:24:07 -0300

    Moved script tag above template for better readability

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,38 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+const isDark = computed(() => colorMode.preference === 'dark')
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  },
+  pending: {
+    type: Boolean,
+    required: true
+  },
+  getUsers: {
+    type: Function as PropType<() => void>,
+    required: true
+  },
+  deleteUsers: {
+    type: Function as PropType<() => void>,
+    required: true
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+const internalQuery = ref(props.modelValue)
+
+watch(internalQuery, (newQuery) => {
+  emit('update:modelValue', newQuery)
+})
+
+function clearQuery() {
+  internalQuery.value = ''
+}
+</script>
+
 <template>
   <div class="flex items-center gap-4">
     <div class="flex flex-wrap items-center justify-between flex-1 gap-2">
@@ -50,38 +85,3 @@
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-const colorMode = useColorMode()
-const isDark = computed(() => colorMode.preference === 'dark')
-
-const props = defineProps({
-  modelValue: {
-    type: String,
-    required: true
-  },
-  pending: {
-    type: Boolean,
-    required: true
-  },
-  getUsers: {
-    type: Function as PropType<() => void>,
-    required: true
-  },
-  deleteUsers: {
-    type: Function as PropType<() => void>,
-    required: true
-  }
-})
-
-const emit = defineEmits(['update:modelValue'])
-const internalQuery = ref(props.modelValue)
-
-watch(internalQuery, (newQuery) => {
-  emit('update:modelValue', newQuery)
-})
-
-const clearQuery = () => {
-  internalQuery.value = ''
-}
-</script>
