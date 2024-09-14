417c499223dae76cedb85108d7d47c9bd5238edb
Thiago Lopes <thiagoslopes8@outlook.com>
2024-09-13 21:59:49 -0300

    Using default loadingIcon on button

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,6 @@
 export default defineAppConfig({
   ui: {
     primary: 'blue',
-    gray: 'zinc',
-    button: {
-      default: {
-        loadingIcon: 'i-svg-spinners-180-ring-with-bg'
-      }
-    }
+    gray: 'zinc'
   }
 })
